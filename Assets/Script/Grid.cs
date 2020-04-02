5183644aca3d7013f49fd57667c037519b277176
athlonneo <athlonneox2@gmail.com>
2020-04-02 23:47:43 +0700

    tile

---
+++ Assets/Script/Grid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public int gridSizeX, gridSizeY;
+    public GameObject tilePrefab;
+    public GameObject[] candies;
+    public GameObject[,] tiles;
+    void Start()
+    {
+        CreateGrid();
+    }
+    void CreateGrid()
+    {
+        Vector2 offset = tilePrefab.GetComponent<SpriteRenderer>().bounds.size;
+        Vector2 startPos = transform.position + (Vector3.left * (offset.x * gridSizeX / 2)) + (Vector3.down * (offset.y * gridSizeY / 3));
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                //Debug.Log(x + ", " + y);
+                Vector2 pos = new Vector3(startPos.x + (x * offset.x), startPos.y + (y * offset.y));
+                GameObject backgroundTile = Instantiate(tilePrefab, pos, tilePrefab.transform.rotation);
+                backgroundTile.transform.parent = transform;
+                backgroundTile.name = "(" + x + "," + y + ")";
+
+                int index = Random.Range(0, candies.Length);
+                GameObject candy = Instantiate(candies[index], pos, Quaternion.identity);
+                //candy.GetComponent<Tile>().Init();
+                candy.name = "(" + x + "," + y + ")";
+                //tiles[x, y] = candy;
+            }
+        }
+    }
+}
---
+++ Assets/Script/Tile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private Vector3 firstPosition;
+    private Vector3 finalPosition;
+    private float swipeAngle;
+
+    void OnMouseDown()
+    {
+        firstPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    void OnMouseUp()
+    {
+        finalPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+    void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalPosition.y - firstPosition.y, finalPosition.x - firstPosition.x) * 180 / Mathf.PI;
+        MoveTile();
+    }
+
+    void MoveTile()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45)
+        {
+            //Right swipe
+            Debug.Log("Right swipe");
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135)
+        {
+            //Up swipe
+            Debug.Log("Up swipe");
+        }
+        else if (swipeAngle > 135 || swipeAngle <= -135)
+        {
+            //Left swipe
+            Debug.Log("Left swipe");
+        }
+        else if (swipeAngle < -45 && swipeAngle >= -135)
+        {
+            //Down swipe
+            Debug.Log("Down swipe");
+        }
+    }
+}
